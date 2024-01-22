4b93bf7c8f45bda0d5febb8c58e74d7e97a2efd1
Volodymyr Donets <volodons@gmail.com>
2024-01-22 16:53:58 +0200

    Create mockup const file list of countries

---
+++ src/components/const/currencies/currencies.js
@@ -0,0 +1,15 @@
+const CURRENCIES = [
+    "UAH",
+    "USD",
+    "EUR",
+    "JPY",
+    "GBP",
+    "CHY",
+    "AUD",
+    "CAD",
+    "CHF",
+    "PLN",
+    "ILS",
+];
+
+export default CURRENCIES;
