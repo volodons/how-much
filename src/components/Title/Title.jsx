621fa09828d794cc6282da0c5da6a3be14cd0452
Volodymyr Donets <volodons@gmail.com>
2024-01-22 16:52:19 +0200

    Refactor and improve Title component

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -3,15 +3,20 @@ import { styled } from "@mui/material";
 import "typeface-lobster";
 
 const StyledTypography = styled(Typography)`
-    margin: 1rem;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
     background-color: #0390fc;
     background: linear-gradient(to right, #0390fc 0%, #8c0dfc 100%);
     background-clip: text;
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
-    font-family: lobster, sans-serif;
+    font-family: Lobster, sans-serif;
     font-size: 5rem;
+    text-align: center;
     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+    &:hover {
+        text-shadow: 3px 3px 6px rgba(0, 0, 0, 0.4);
+    }
 `;
 
 const Title = () => {
