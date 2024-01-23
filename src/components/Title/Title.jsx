b91f9e811de896f8c8444b721214925d965fe974
Volodymyr Donets <volodons@gmail.com>
2024-01-23 17:02:34 +0200

    Refactor Title component

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -3,15 +3,10 @@ import { styled } from "@mui/material";
 import "typeface-lobster";
 
 const StyledTypography = styled(Typography)`
-    margin-top: 2rem;
     margin-bottom: 2rem;
-    background-color: #0390fc;
-    background: linear-gradient(to right, #0390fc 0%, #8c0dfc 100%);
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
+    color: #f5f7ff;
     font-family: Lobster, sans-serif;
-    font-size: 5rem;
+    font-size: 5.625rem;
     text-align: center;
     text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
     &:hover {
