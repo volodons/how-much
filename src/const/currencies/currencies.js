199a52e117df1a009e427bbbf3ae804d57614477
Volodymyr Donets <volodons@gmail.com>
2024-01-24 12:00:05 +0200

    Update currencies file data

--- src/const/currencies/currencies.js
+++ src/const/currencies/currencies.js
@@ -1,15 +1,59 @@
 const CURRENCIES = [
-    "UAH",
-    "USD",
-    "EUR",
-    "JPY",
-    "GBP",
-    "CHY",
-    "AUD",
-    "CAD",
-    "CHF",
-    "PLN",
-    "ILS",
+    {
+        id: 1,
+        currency: "UAH",
+        country: "UKR",
+    },
+    {
+        id: 2,
+        currency: "USD",
+        country: "USA",
+    },
+    {
+        id: 3,
+        currency: "EUR",
+        country: "EU",
+    },
+    {
+        id: 4,
+        currency: "JPY",
+        country: "JPN",
+    },
+    {
+        id: 5,
+        currency: "GBP",
+        country: "GBR",
+    },
+    {
+        id: 6,
+        currency: "CHY",
+        country: "CHN",
+    },
+    {
+        id: 7,
+        currency: "AUD",
+        country: "AUS",
+    },
+    {
+        id: 8,
+        currency: "CAD",
+        country: "CAN",
+    },
+    {
+        id: 9,
+        currency: "CHF",
+        country: "CHE",
+    },
+    {
+        id: 10,
+        currency: "PLN",
+        country: "POL",
+    },
+    {
+        id: 11,
+        currency: "ILS",
+        country: "ISR",
+    },
 ];
 
 export default CURRENCIES;
