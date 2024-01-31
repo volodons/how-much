4c58ec250e03860fe20f812f11a688ac16881269
Volodymyr Donets <volodons@gmail.com>
2024-01-31 09:17:02 +0200

    Put API call URL into const

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
+const FETCH_ALL_CURRENCIES_URL =
+    'https://restcountries.com/v3.1/all?fields=currencies,cca3';
+
 export const fetchAllCurrencies = async () => {
-    const allCurrencies = await axios.get(
-        'https://restcountries.com/v3.1/all?fields=currencies,cca3'
-    );
-    console.log(allCurrencies);
+    const allCurrencies = await axios.get(`${FETCH_ALL_CURRENCIES_URL}`);
     const uniqueCurrencies = [
         ...new Set(
             allCurrencies.data.map((item) => Object.keys(item.currencies)[0])
